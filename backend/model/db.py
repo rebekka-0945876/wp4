bdc5cdd713277862807a142aba4938732dacb366
rebekka-0945876 <143963944+rebekka-0945876@users.noreply.github.com>
2024-09-22 23:31:31 +0200

    Logfunctie gemaakt + database tabel aangemaakt

--- backend/model/db.py
+++ backend/model/db.py
@@ -1,7 +1,23 @@
 import sqlite3
+from flask import request
+import datetime
 
 
 def get_db_connection():
     connection = sqlite3.connect('./db-presentation.sqlite3')
     connection.row_factory = sqlite3.Row
     return connection
+
+
+def log_event(user_id, event_type, description):
+    conn = get_db_connection()
+    ip_address = request.remote_addr
+    timestamp = datetime.datetime.now()
+
+    conn.execute("""
+        INSERT INTO AuditLogs (user_id, event_type, description, ip_address, timestamp)
+        VALUES (?, ?, ?, ?, ?)
+    """, (user_id, event_type, description, ip_address, timestamp))
+
+    conn.commit()
+    conn.close()
